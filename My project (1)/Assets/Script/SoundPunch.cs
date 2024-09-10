5427981e6a4ccc1b74469bf48b2b4b8e09241026
PorYossawin <yossawin.kasi@bumail.net>
2024-09-11 00:13:44 +0700

    sound

---
+++ My project (1)/Assets/Script/SoundPunch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPunch : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // ดึง AudioSource จาก GameObject ที่สคริปต์นี้ถูกแนบอยู่
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // ตรวจสอบเมื่อผู้เล่นคลิกซ้าย (Mouse Button 0 คือคลิกซ้าย)
+        if (Input.GetMouseButtonDown(0))
+        {
+            // เล่นเสียง
+            audioSource.Play();
+        }
+    }
+}
